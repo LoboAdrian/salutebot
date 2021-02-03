6d3ddca6be769d7dbbd0d367fdaf3e04f3b77205
Adrian Lobo <alf8aml04@gmail.com>
2021-02-02 20:30:09 -0500

    hoping it works again

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ def message_salute(ack, say, command, message):
     # say() sends a message to the channel where the event was triggered
     say(f":salute::salute::salute::salute::salute::salute::salute:\n:salute::salute::salute::salute::salute::salute::salute:\n:salute::salute::salute::salute::salute::salute::salute:")
     say(f":salute:")
-    say(f"\\\n   "<@{command['user']}>") PRESENT ARMS!")
-
 
 # Start your app
 if __name__ == "__main__":
