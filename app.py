77d594e7e6e3803196366e94178c17e74dc9ba36
Adrian Lobo <alf8aml04@gmail.com>
2021-02-03 11:08:57 -0500

    toitles again part 1

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ def corgi_salute(ack, say, command, message):
     say(f":corgi::corgi::corgi::corgi::corgi::corgi::corgi:\n:corgi::corgi::corgi::corgi::corgi::corgi::corgi:\n:corgi::corgi::corgi::corgi::corgi::corgi::corgi:\n:salute:")
     say(f"LONG LIVE THE CORGI STATES")
 
+@app.command("turtlesalute")
+def turtle_salute(ack, say, command, message):
+    say(f":soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle:\n:soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle:\n:soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle: :soviet-turtle:\n:salute:")
+
 # Start your app
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
